d7aaaccaed6b4f7321a8b99972f5eebd4c782422
Ameliarate16 <an.morton16@gmail.com>
2025-03-31 00:15:48 -0400

    Fixed scale and click position for entity spawning

--- scripts/input_controller.gd
+++ scripts/input_controller.gd
@@ -19,27 +19,28 @@ func _process(delta):
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			click_position = event.position
-			var newPlant
-			
-			if Global.selected == Global.SeedType.HERB:
-				var herbType = randi_range(0, herbs.size() -1)
-				newPlant = herbs[herbType].instantiate()
-			elif Global.selected == Global.SeedType.MUSH:
-				var mushType = randi_range(0, mushrooms.size() -1)
-				newPlant = mushrooms[mushType].instantiate()
-				return
-			elif Global.selected == Global.SeedType.BUSH:
-				var bushType = randi_range(0, bushes.size() -1)
-				newPlant = bushes[bushType].instantiate()
-				return
-			elif Global.selected == Global.SeedType.TREE:
-				var treeType = randi_range(0, trees.size() -1)
-				newPlant = trees[treeType].instantiate()
-				return
-			else:
-				return
-			
-			newPlant.setPos(click_position)
-			add_child(newPlant)
-			createPlant.emit(newPlant.getUpkeep())
+		click_position = event.position - Vector2(get_viewport().size)/8
+		click_position.y = Global.groundY
+		var newPlant
+		
+		if Global.selected == Global.SeedType.HERB:
+			var herbType = randi_range(0, herbs.size() -1)
+			newPlant = herbs[herbType].instantiate()
+		elif Global.selected == Global.SeedType.MUSH:
+			var mushType = randi_range(0, mushrooms.size() -1)
+			newPlant = mushrooms[mushType].instantiate()
+			return
+		elif Global.selected == Global.SeedType.BUSH:
+			var bushType = randi_range(0, bushes.size() -1)
+			newPlant = bushes[bushType].instantiate()
+			return
+		elif Global.selected == Global.SeedType.TREE:
+			var treeType = randi_range(0, trees.size() -1)
+			newPlant = trees[treeType].instantiate()
+			return
+		else:
+			return
+		
+		newPlant.setPos(click_position)
+		add_child(newPlant)
+		createPlant.emit(newPlant.getUpkeep())
